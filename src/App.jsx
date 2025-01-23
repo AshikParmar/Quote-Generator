4cbfaa3251790458fab72e20875dab2877531a85
Ashik Parmar <ashikparmar5123@gmail.com>
2025-01-23 19:54:04 +0530

    update fetch using async await

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,38 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count,setCount] = useState(true);
-  const [data, setData] = useState({});
+  const [count,setCount] = useState(false);
+  const [data, setData] = useState("");
 
   const handleClick = () => {
-    setCount( !count);
+    setCount(count => !count);
   };
 
   useEffect(()=>{
 
-    fetch('https://thequoteshub.com/api/')
-    .then((response)=> response.json() )
-    .then((response)=> setData(response))
-    .catch((error)=> console.log(error))
+    fetchdata();
+
+    // fetch('https://thequoteshub.com/api/')
+    // .then((response)=> response.json() )
+    // .then((response)=> setData(response))
+    // .catch((error)=> console.log(error))
     
     
   },[count]);
+
+  const fetchdata = async() => {
+    const response = await fetch('https://thequoteshub.com/api/');
+
+    const result = await response.json();
+
+    setData(result.text);
+  }
 
   return (
     <div>
       <div id='container'>
         <h2>Quote Generator</h2>
-        <p>{data.text}</p>
+        <p>{data}</p>
         <button onClick={handleClick}>Generate</button>
       </div>
     </div>
